51224ea30552d0019efdbc6c455a372892f8758c
Jenitta17 <jeniwinecity@gmail.com>
2024-08-07 22:53:49 +0530

    first commit

---
+++ twosum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twosum(self, nums, target):
+        numbs = list(enumerate(nums))
+        numbs.sort(key=lambda x: x[1])
+        l = 0
+        r = len(nums) - 1
+        
+        while l < r:
+            curr = numbs[l][1] + numbs[r][1]
+            if curr > target:
+                r -= 1
+            elif curr < target:
+                l += 1
+            else:
+                return [numbs[l][0], numbs[r][0]]
+        return []
